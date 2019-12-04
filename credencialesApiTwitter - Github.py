002884289bf5b85779e5341197752600b237fa6e
eljimenezj2 <57123883+eljimenezj2@users.noreply.github.com>
2019-12-03 21:57:24 -0500

    Add files via upload
    Jupyter 1 - Utilizando tweepy para extracción de datos de la API de Twitter

---
+++ credencialesApiTwitter - Github.py
@@ -0,0 +1,8 @@
+# Credenciales De Acceso para el Consumo de la api de Twiter.
+
+accessToken = "INSERT YOUR ACCESS TOKEN HERE"
+accessSecret = "INSERT YOUR ACCESS SECRET HERE"
+consumerkey = "INSERT YOUR CONSUMER KEY HERE"
+consumerSecret = "INSERT YOUR CONSUMER SECRET HERE"
+
+access_token_secret= 'INSERT YOUR ACCESS TOKEN SECRET HERE'
